a86be45541541bfc43ad9539cca35ca864ce9264
me76210356ltkm5 <48250055+me76210356ltkm5@users.noreply.github.com>
2019-03-24 20:51:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-$(function() {
-  
-  function toggleChangeBtn() {
-    var slideIndex = $('.slide').index($('.active'));
-    $('.change-btn').show();
-    if (slideIndex == 0) {
-      $('.prev-btn').hide();
-    // 「3」の部分を、lengthメソッドを用いて書き換えてください
-    } else if (slideIndex == ($('.slide').length-1)) {
-      $('.next-btn').hide();
-    }
-  }
-  
-  $('.index-btn').click(function() {
-    $('.active').removeClass('active');
-    var clickedIndex = $('.index-btn').index($(this));
-    $('.slide').eq(clickedIndex).addClass('active');
-    toggleChangeBtn();
-  });
-  
-  $('.change-btn').click(function() {
-    var $displaySlide = $('.active');
-    $displaySlide.removeClass('active');
-    if ($(this).hasClass('next-btn')) {
-      $displaySlide.next().addClass('active');
-    } else {
-      $displaySlide.prev().addClass('active');
-    }
-    toggleChangeBtn();
+$(function(){
+  (function ($) {
+
+  	'use strict';
+
+      let scrollPosi = 0;
+
+  	$(window).scroll(function () {
+  		scrollPosi = $(document).scrollTop();
+
+          $('eight-wrapper').stop(true, true).animate({
+  			'background-position-y': -scrollPosi / 3 + 'px'
+  		}, 100);
+  	});
+
   });
 });
