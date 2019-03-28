ae4134f0da0096e5cd8a834aae5c935e42940726
me76210356ltkm5 <48250055+me76210356ltkm5@users.noreply.github.com>
2019-03-29 02:46:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 $(function(){
-  (function ($) {
-
-  	'use strict';
-
-      let scrollPosi = 0;
-
-  	$(window).scroll(function () {
-  		scrollPosi = $(document).scrollTop();
-
-          $('eight-wrapper').stop(true, true).animate({
-  			'background-position-y': -scrollPosi / 3 + 'px'
-  		}, 100);
-  	});
-
+  var target1 = $(".eight-wrapper");
+  var targetPosOT1 = target1.offset().top;
+  var targetFactor = -0.5;
+  var windowH = $(window).height();
+  var scrollYStart = targetPosOT1 - windowH;
+  $(window).on('scroll',function(){
+    var scrollY = $(this).scrollTop();
+    if(scrollY > scrollYStart){
+      target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+    }else{
+      target1.css('background-position','center top');}
   });
 });
