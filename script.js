0696a769f4d993ba6f0c45c5968f363d85624976
me76210356ltkm5 <48250055+me76210356ltkm5@users.noreply.github.com>
2019-04-08 00:34:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ $(function(){
       target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
     }else{
       target1.css('background-position','center top');}
+    var homeTag = $(".Home-tag");
+    var accessTag = $(".Access-tag");
+    var reservationTag = $(".Reservation-tag");
+    var recruitTag = $(".Recruit-tag");
+    var sixthWrapper = $(".sixth-wrapper").offset().top;
+    var seventhWrapper = $(".seventh-wrapper").offset().top;
+    var ninthWrapper = $(".ninth-wrapper").offset().top;
+
+    if (scrollY <= sixthWrapper) {
+      homeTag.addClass('Select-tag').removeClass('tag');
+    }
+    if (scrollY > sixthWrapper && scrollY <= seventhWrapper) {
+      homeTag.addClass('tag').removeClass('Select-tag'), accessTag.addClass('Select-tag').removeClass('tag');
+    } else {
+      accessTag.addClass('tag').removeClass('Select-tag');
+    }
+    if (scrollY > seventhWrapper && scrollY <= ninthWrapper) {
+      accessTag.addClass('tag').removeClass('Select-tag'), reservationTag.addClass('Select-tag').removeClass('tag');
+    } else {
+      reservationTag.addClass('tag').removeClass('Select-tag');
+    }
+    if (scrollY > ninthWrapper) {
+      reservationTag.addClass('tag').removeClass('Select-tag'), recruitTag.addClass('Select-tag').removeClass('tag');
+    } else {
+      recruitTag.addClass('tag').removeClass('Select-tag');
+    }
   });
 });
