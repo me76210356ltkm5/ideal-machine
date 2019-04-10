abc875006a8e3505e3ca39d3bdba0900b2e833d8
me76210356ltkm5 <48250055+me76210356ltkm5@users.noreply.github.com>
2019-04-11 00:19:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
-$(function(){
+$(function() {
   var target1 = $(".eight-wrapper");
   var targetPosOT1 = target1.offset().top;
-  var targetFactor = -0.5;
+  var targetFactor = -0.2;
   var windowH = $(window).height();
   var scrollYStart = targetPosOT1 - windowH;
-  $(window).on('scroll',function(){
-    var scrollY = $(this).scrollTop();
-    if(scrollY > scrollYStart){
-      target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
-    }else{
-      target1.css('background-position','center top');}
-    var homeTag = $(".Home-tag");
-    var accessTag = $(".Access-tag");
-    var reservationTag = $(".Reservation-tag");
-    var recruitTag = $(".Recruit-tag");
-    var sixthWrapper = $(".sixth-wrapper").offset().top;
-    var seventhWrapper = $(".seventh-wrapper").offset().top;
-    var ninthWrapper = $(".ninth-wrapper").offset().top;
+  var homeTag = $(".Home-tag");
+  var accessTag = $(".Access-tag");
+  var reservationTag = $(".Reservation-tag");
+  var recruitTag = $(".Recruit-tag");
+  var sixthWrapper = $(".sixth-wrapper").offset().top;
+  var seventhWrapper = $(".seventh-wrapper").offset().top;
+  var ninthWrapper = $(".ninth-wrapper").offset().top;
 
-    if (scrollY <= sixthWrapper) {
+
+  $(window).on('scroll', function() {
+    var scrollY = $(this).scrollTop();
+    if (scrollY > scrollYStart) {
+      target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+    } else {
+      target1.css('background-position', 'center top');
+    }
+
+
+    if (scrollY <= sixthWrapper-1) {
       homeTag.addClass('Select-tag').removeClass('tag');
     }
-    if (scrollY > sixthWrapper && scrollY <= seventhWrapper) {
+    if (scrollY > sixthWrapper-1 && scrollY <= seventhWrapper-1) {
       homeTag.addClass('tag').removeClass('Select-tag'), accessTag.addClass('Select-tag').removeClass('tag');
     } else {
       accessTag.addClass('tag').removeClass('Select-tag');
     }
-    if (scrollY > seventhWrapper && scrollY <= ninthWrapper) {
+    if (scrollY > seventhWrapper-1 && scrollY <= ninthWrapper-1) {
       accessTag.addClass('tag').removeClass('Select-tag'), reservationTag.addClass('Select-tag').removeClass('tag');
     } else {
       reservationTag.addClass('tag').removeClass('Select-tag');
     }
-    if (scrollY > ninthWrapper) {
+    if (scrollY > ninthWrapper-1) {
       reservationTag.addClass('tag').removeClass('Select-tag'), recruitTag.addClass('Select-tag').removeClass('tag');
     } else {
       recruitTag.addClass('tag').removeClass('Select-tag');
     }
-  });
+  })
+  homeTag.click(function(){
+    $("html,body").animate({scrollTop:0},{duration:1000});
+  })
+  accessTag.click(function(){
+    $("html,body").animate({scrollTop:sixthWrapper},{duration:1000});
+  })
+  reservationTag.click(function(){
+    $("html,body").animate({scrollTop:seventhWrapper},{duration:1000});
+  })
+  recruitTag.click(function(){
+    $("html,body").animate({scrollTop:ninthWrapper},{duration:1000});
+  })
+
 });
